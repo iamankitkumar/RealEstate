e534a63cbdad7157ed7a986ccc479fe3548e3e7e
iamankitkumar <yadavankitkumar6600@gmail.com>
2024-11-16 00:18:31 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './Conponents/Navbar'
 import Header from './Conponents/Header'
+import About from './Conponents/About'
 
 const App = () => {
   return (
     <div className='w-full overflow-hidden'>
       <Header/>
+      <About/>
     </div>
   )
 }
